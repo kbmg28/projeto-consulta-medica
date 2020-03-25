c106699a867957d731fd76367303cdf27bd19047
kevin-gomes <kevin28bruno@gmail.com>
2020-03-25 18:57:48 -0400

    pessoadto

---
+++ ws-consulta-medica/src/main/java/br/com/kbmg/wsconsultamedica/dto/PessoaDto.java
@@ -0,0 +1,35 @@
+package br.com.kbmg.wsconsultamedica.dto;
+
+public class PessoaDto {
+
+	private String idPessoa;
+	private String nomeCompleto;
+	private String email;
+	private String senha;
+	
+	public String getIdPessoa() {
+		return idPessoa;
+	}
+	public void setIdPessoa(String idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}

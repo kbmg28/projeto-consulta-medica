a728acd9fcc6fd91690f1aa1b14f0cf1fd6cc05a
kevin-gomes <kevin28bruno@gmail.com>
2020-03-26 13:57:44 -0400

    [front] app component update

--- front-consulta-medica/src/app/app.component.ts
+++ front-consulta-medica/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  constructor(private authService: AuthService) {}
+
   title = 'front-consulta-medica';
+
+  ngOnInit() {
+    this.authService.autoLogin();
+  }
 }
